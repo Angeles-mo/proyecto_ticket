753633dd32a4f9671626b69762af049614c926c2
Angeles <angelamonicanr1@gmail.com>
2026-02-16 22:15:12 +0100

    Proyecto de springboot

---
+++ Ticketworld/src/main/java/com/ticketworld/Ticketworld/TicketworldApplication.java
@@ -0,0 +1,13 @@
+package com.ticketworld.Ticketworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketworldApplication.class, args);
+	}
+
+}
---
+++ Ticketworld/src/test/java/com/ticketworld/Ticketworld/TicketworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketworld.Ticketworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
