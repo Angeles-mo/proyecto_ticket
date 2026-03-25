4c69fbda68ced2d2fbb52f170c83995f6687bf29
Angeles <angelamonicanr1@gmail.com>
2026-03-25 12:31:43 +0100

    Captura de pantallas - Login

--- Frontend/Ticketworld/src/app/components/login/login.ts
+++ Frontend/Ticketworld/src/app/components/login/login.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onLogin() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('role', response.role);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.errorMessage = 'Email o contraseña incorrectos';
+        console.error(err);
+      }
+    });
+  }
+}
